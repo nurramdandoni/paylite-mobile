c194d38488761152adb3aac46ec2762d1d4f3bc0
Doni Nurramdan <nurramdandoni@gmail.com>
2023-07-14 10:52:38 +0700

    add instance retrofit

--- app/src/main/java/com/example/paylite_mobile/helper/retrofit/RetrofitHelper.kt
+++ app/src/main/java/com/example/paylite_mobile/helper/retrofit/RetrofitHelper.kt
@@ -1,4 +1,14 @@
 package com.example.paylite_mobile.helper.retrofit
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitHelper {
+    private const val baseurl = "https://api.paylite.co.id"
+
+    fun getInstance(): Retrofit {
+        return Retrofit.Builder().baseUrl(baseurl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
